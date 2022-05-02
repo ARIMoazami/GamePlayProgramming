a7e5000d87113900a04b43c91470969189aba002
Sebastian A <Sebadam2010@hotmail.co.uk>
2022-05-02 16:13:00 +0100

    Manual merge from Seb_dev to FinalComponent

--- GameplayProgramming/Assets/Seb_Assets/Scripts/Powerup_StrongAttack.cs
+++ GameplayProgramming/Assets/Seb_Assets/Scripts/Powerup_StrongAttack.cs
@@ -7,10 +7,10 @@ public class Powerup_StrongAttack : Powerup
 {
 
     public GameObject UI_image_go;
-    [Range(5.0f, 15.0f)]
-    public float duration = 10.0f;
+    public float duration = 3.0f;
 
-    private Image UI_image;
+    private GameObject strength_text_go;
+    private Text strength_text_txt;
 
     private GameObject player;
     private PlayerMovement player_script;
@@ -20,32 +20,32 @@ public class Powerup_StrongAttack : Powerup
         player = GameObject.FindWithTag("Player");
         player_script = player.GetComponent<PlayerMovement>();
 
-        UI_image_go = GameObject.FindWithTag("StrongAttackImage");
-        UI_image = UI_image_go.GetComponent<Image>();
-        UI_image_go.SetActive(false);
+        strength_text_go = GameObject.FindWithTag("StrengthText");
+        strength_text_txt = strength_text_go.GetComponent<Text>();
 
-        Debug.Log("Started powerup_strongAttack");
+        strength_text_go.SetActive(false);
+
+    Debug.Log("Started powerup_strongAttack");
     }
 
-    public IEnumerator StrongAttack()
+    public void StrongAttack()
     {
         enablePowerup();
-        yield return new WaitForSeconds(duration);
-        disablePowerup();
+        
     }
 
     public override void enablePowerup()
     {
         player_script.setStrongAttack(true);
-        UI_image_go.SetActive(true);
-        UI_image.canvasRenderer.SetAlpha(1.0f);
-        UI_image.CrossFadeAlpha(0.0f, duration, false);
+        strength_text_go.SetActive(true);
+        strength_text_txt.canvasRenderer.SetAlpha(1.0f);
+        strength_text_txt.CrossFadeAlpha(0.0f, duration, false);
+
     }
 
     public override void disablePowerup()
     {
         Debug.Log("Disabled Powerup");
         player_script.setStrongAttack(false);
-        UI_image_go.SetActive(false);
     }
 }
